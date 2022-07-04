0ad8622f343592f375d5233f3612e39393714579
18318180911 <80408028+18318180911@users.noreply.github.com>
2022-07-04 11:14:56 +0800

    练习

--- day02-1/pages/index/index.js
+++ day02-1/pages/index/index.js
@@ -1,4 +1,7 @@
 Page({
+  data: {
+    htmlStr: '<h1 class="color" id="h1">大标题<h1>'
+  },
   phoneNumber(e) {
     console.log(e)
     // 怎么拿到:e.detail.code(code:是通过加密的手机号)
--- day02-1/pages/practice/practice.js
+++ day02-1/pages/practice/practice.js
@@ -1,2 +1,13 @@
 // pages/practice/practice.js
-Page({})
+Page({
+  data: {
+    currentIndex: 0,
+    list: ['苹果', '香蕉', '龙眼', '雪梨', '榴莲', '西瓜', '蓝莓', '草莓', '芒果']
+  },
+  headlerClick(e) {
+    console.log(e)
+    this.setData({
+      currentIndex: e.target.dataset.index
+    })
+  }
+})
