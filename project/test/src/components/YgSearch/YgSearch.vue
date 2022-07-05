c076c69bb4d43149de22ea19190c1a18dba72b09
18318180911 <80408028+18318180911@users.noreply.github.com>
2022-07-05 20:15:28 +0800

    首页的搜索和轮播图
    首页的搜索和轮播图

---
+++ project/test/src/components/YgSearch/YgSearch.vue
@@ -0,0 +1,22 @@
+<template>
+  <view class="yg-search">
+    <u-search
+      placeholder="搜索"
+      shape="square"
+      :show-action="false"
+      input-align="center"
+      bg-color="#fff"
+    ></u-search>
+  </view>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.yg-search {
+  background-color: #ea4350;
+  padding: 15rpx;
+}
+</style>
--- project/test/src/pages/index/index.vue
+++ project/test/src/pages/index/index.vue
@@ -1,12 +1,32 @@
 <template>
 	<view>
-		首页
+		<YgSearch></YgSearch>
+		<u-swiper :list="swiperList" name="image_src" height="340"></u-swiper>
 	</view>
 </template>
 
 <script>
 	export default {
-	}
+  data() {
+    return {
+      swiperList: [],
+    };
+  },
+  onLoad() {
+    // 获取轮播图
+    this.getSwiperData();
+  },
+  methods: {
+    async getSwiperData() {
+      const [err, data] = await uni.request({
+        url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata",
+      });
+      console.log(err);
+      console.log(data);
+      this.swiperList = data.data.message;
+    },
+  },
+};
 </script>
 
 <style>
