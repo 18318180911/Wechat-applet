12dd1f2addcc6427886b1b991ae6cb4b1f94602f
18318180911 <80408028+18318180911@users.noreply.github.com>
2022-07-05 17:56:08 +0800

    完成首页轮播图
    完成首页轮播图

--- project/my-project/src/pages/index/index.vue
+++ project/my-project/src/pages/index/index.vue
@@ -1,10 +1,34 @@
 <template>
-	<view><YgSearch></YgSearch></view>
+  <view>
+    <!-- 1.搜索框 -->
+    <YgSearch></YgSearch>
+    <!-- 2.轮播图 -->
+    <u-swiper :list="swiperList" name="image_src" height="340"></u-swiper>
+  </view>
 </template>
 
 <script>
-	export default {
-	}
+export default {
+  data() {
+    return {
+      swiperList: [],
+    };
+  },
+  onLoad() {
+    // 获取轮播图
+    this.getSwiperData();
+  },
+  methods: {
+    async getSwiperData() {
+      const [err, data] = await uni.request({
+        url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata",
+      });
+      console.log(err);
+      console.log(data);
+      this.swiperList = data.data.message;
+    },
+  },
+};
 </script>
 
 <style>
