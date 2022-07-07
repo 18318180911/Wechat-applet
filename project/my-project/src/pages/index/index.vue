68517594dca5d8a4edb2c18a92c47a85edba587e
18318180911 <80408028+18318180911@users.noreply.github.com>
2022-07-07 10:39:14 +0800

    导航栏的完成z
    导航栏的完成z

--- project/my-project/src/pages/index/index.vue
+++ project/my-project/src/pages/index/index.vue
@@ -4,6 +4,12 @@
     <YgSearch></YgSearch>
     <!-- 2.轮播图 -->
     <u-swiper :list="swiperList" name="image_src" height="340"></u-swiper>
+    <!-- 导航栏 -->
+    <view class="nav u-flex">
+      <navigator v-for="item in catitemsList" :key="item.name" class="u-flex-1 u-p-20">
+        <u-image :src="item.image_src" width="100%" mode="widthFix"></u-image>
+      </navigator>
+    </view>
   </view>
 </template>
 
@@ -11,24 +17,30 @@
 export default {
   data() {
     return {
+      // 轮播图
       swiperList: [],
+      // 导航栏
+      catitemsList: []
     };
   },
   onLoad() {
     // 获取轮播图
     this.getSwiperData();
+    // 获取导航栏
+    this.getcatItems();
   },
   methods: {
+    // 获取轮播图
     async getSwiperData() {
-    //   const [err, data] = await uni.request({
-    //     url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata",
-    //   });
-    //   console.log(err);
-    //   console.log(data);
-    //   this.swiperList = data.data.message;
 		const result = await this.$u.get("/home/swiperdata")
-		console.log(result)
+		// console.log(result)
 		this.swiperList = result.message;
+    },
+    // 获取导航栏数据
+    async getcatItems() {
+		const result = await this.$u.get("/home/catitems")
+		console.log(42, result)
+		this.catitemsList = result.message;
     },
   },
 };
