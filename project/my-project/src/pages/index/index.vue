c4b8b3de8fa25401f7a740c57ffeadc0cc03d9fb
18318180911 <80408028+18318180911@users.noreply.github.com>
2022-07-07 10:55:04 +0800

    楼层模块的实现
    楼层模块的实现

--- project/my-project/src/pages/index/index.vue
+++ project/my-project/src/pages/index/index.vue
@@ -10,6 +10,38 @@
         <u-image :src="item.image_src" width="100%" mode="widthFix"></u-image>
       </navigator>
     </view>
+    <!-- 楼层 -->
+    <view class="floor">
+      <!-- 4.1 一层遍历 -->
+      <view
+        class="floor-item"
+        v-for="item1 in floorDatas"
+        :key="item1.floor_title.name"
+      >
+        <view class="floor-title">
+          <!-- uview 默认宽度和高度  -->
+          <u-image
+            width="100%"
+            mode="widthFix"
+            :src="item1.floor_title.image_src"
+          ></u-image>
+        </view>
+        <view class="product-list">
+          <!-- 2 层遍历 -->
+          <navigator
+            v-for="item2 in item1.product_list"
+            :key="item2.name"
+          >
+            <u-image
+              width="100%"
+              height="100%"
+              mode="scallToFill"
+              :src="item2.image_src"
+            ></u-image>
+          </navigator>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -20,7 +52,9 @@ export default {
       // 轮播图
       swiperList: [],
       // 导航栏
-      catitemsList: []
+      catitemsList: [],
+      // 楼层
+      floorDatas: []
     };
   },
   onLoad() {
@@ -28,6 +62,8 @@ export default {
     this.getSwiperData();
     // 获取导航栏
     this.getcatItems();
+    // 楼层
+    this.getfloorData()
   },
   methods: {
     // 获取轮播图
@@ -42,9 +78,37 @@ export default {
 		console.log(42, result)
 		this.catitemsList = result.message;
     },
+    // 获取楼层的数据
+    
+    async getfloorData() {
+		const result = await this.$u.get("/home/floordata")
+		console.log(54, result)
+		this.floorDatas = result.message;
+    },
   },
 };
 </script>
 
-<style>
+<style lang="scss">
+.product-list {
+  // 清除浮动
+  overflow: hidden;
+  // 公共样式
+  navigator {
+    // 750rpx 250rpx     100vw
+    width: 232rpx;
+    float: left;
+
+    margin: 0 8.6rpx;
+    margin-bottom: 8.6rpx;
+  }
+  // 第一个 导航标签 高度
+  navigator:first-child {
+    height: 386rpx;
+  }
+  // 后四个导航标签 的高度
+  navigator:nth-last-child(-n + 4) {
+    height: 188rpx;
+  }
+}
 </style>
