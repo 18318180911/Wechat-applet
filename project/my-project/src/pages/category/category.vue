910d56edc60dab3ec2741907d742620251c7ab3c
18318180911 <80408028+18318180911@users.noreply.github.com>
2022-07-07 11:18:40 +0800

    商品分类-左侧菜单动态渲染
    商品分类-左侧菜单动态渲染

--- project/my-project/src/pages/category/category.vue
+++ project/my-project/src/pages/category/category.vue
@@ -4,7 +4,9 @@
     <YgSearch></YgSearch>
     <!-- 2 内容 -->
     <view class="main">
-      <view class="menu">1</view>
+      <view class="menu">
+        <view :class="['menu-item',index===0&&'active']" v-for="(item, index) in menuList" :key="item">{{item}}</view>
+      </view>
       <view class="content">2</view>
     </view>
   </view>
@@ -12,6 +14,23 @@
 
 <script>
 export default {
+  data() {
+    return {
+      // 左边标题数组
+      menuList: []
+    }
+  },
+  onLoad() {
+    this.getCategories()
+  },
+  methods:{
+    async getCategories() {
+      const result = await this.$u.get("/categories")
+      console.log(25, result)
+      // this.menuList = result.message
+      this.menuList = result.message.map((item) => item.cat_name);
+    }
+  }
 
 }
 </script>
@@ -23,6 +42,28 @@ export default {
   .menu {
     width: 182rpx;
     overflow: auto;
+    .menu-item {
+      height: 70rpx;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 28rpx;
+      color: #585858;
+    }
+    .active {
+      color: #ea4350;
+      position: relative;
+      &::before {
+        content: "";
+        width: 5rpx;
+        height: 60%;
+        position: absolute;
+        left: 0;
+        top: 50%;
+        transform: translateY(-50%);
+        background-color: #ea4350;
+      }
+    }
   }
   .content {
     flex: 1;
