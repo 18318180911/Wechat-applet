efda2bc72188c9c1c19fc55615424b4b51d74466
18318180911 <80408028+18318180911@users.noreply.github.com>
2022-07-08 11:16:44 +0800

    商品详情-轮播图
    商品详情-轮播图

--- project/my-project/src/pages/goods_detail/goods_detail.vue
+++ project/my-project/src/pages/goods_detail/goods_detail.vue
@@ -1,32 +1,40 @@
 <template>
   <view>
+    <!-- 1.轮播图 -->
+    <u-swiper
+      :list="detailList.pics"
+      name="pics_big"
+      height="497"
+      img-mode="aspectFit"
+      bg-color="#fff"
+    ></u-swiper>
   </view>
 </template>
 
 <script>
-import { option } from '@dcloudio/vue-cli-plugin-uni/packages/postcss/tags'
+import { option } from "@dcloudio/vue-cli-plugin-uni/packages/postcss/tags";
 export default {
-    data() {
-        return {
-            detailList: []
-        }
+  data() {
+    return {
+      // 商品详情对象
+      detailList: [],
+    };
+  },
+  onLoad() {
+    const goods_id = option.goods_id || 43984;
+    console.log(17, goods_id);
+    this.getDetail(goods_id);
+  },
+  methods: {
+    // 获取商品详情数据
+    async getDetail(goods_id) {
+      const result = await this.$u.get("/goods/detail", { goods_id });
+      console.log(15, result);
+      this.detailList = result.message;
     },
-    onLoad() {
-        const goods_id = option.goods_id || 5
-        console.log(17, goods_id)
-        this.getDetail(goods_id)
-    },
-    methods: {
-        // 获取商品详情数据
-        async getDetail(goods_id) {
-            const result = await this.$u.get("/goods/detail?cid=5", {goods_id})
-            console.log(15, result)
-            this.detailList = result.message.detailList
-        }
-    }
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
