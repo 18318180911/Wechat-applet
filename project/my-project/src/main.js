ea960e804fe2b5610b0a4c12689cb880902c98bd
18318180911 <80408028+18318180911@users.noreply.github.com>
2022-07-08 15:19:00 +0800

    vuex的配置和引入和引用
    vuex的配置和引入和引用

--- project/my-project/src/main.js
+++ project/my-project/src/main.js
@@ -2,12 +2,16 @@ import Vue from 'vue'
 import App from './App'
 import uView from 'uview-ui';
 Vue.use(uView);
+import store from './store'
+
+Vue.prototype.$store = store
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
+  store,
   ...App
 })
 // http拦截器，此为需要加入的内容，如果不是写在common目录，请自行修改引入路径
--- project/my-project/src/pages/goods_detail/goods_detail.vue
+++ project/my-project/src/pages/goods_detail/goods_detail.vue
@@ -17,6 +17,45 @@
     >
     <!-- 图片详情 -->
      <u-parse :html="detailList.goods_introduce"></u-parse>
+     <!-- 4 购物车工具栏 -->
+    <view class="navigation">
+      <view class="left">
+        <view class="item">
+          <u-icon
+            name="server-fill"
+            :size="40"
+            :color="$u.color['contentColor']"
+          ></u-icon>
+          <view class="text u-line-1">客服</view>
+        </view>
+        <view class="item">
+          <u-icon
+            name="home"
+            :size="40"
+            :color="$u.color['contentColor']"
+          ></u-icon>
+          <view class="text u-line-1">店铺</view>
+        </view>
+        <view class="item car">
+          <u-badge
+            class="car-num"
+            :count="9"
+            type="error"
+            :offset="[-3, -6]"
+          ></u-badge>
+          <u-icon
+            name="shopping-cart"
+            :size="40"
+            :color="$u.color['contentColor']"
+          ></u-icon>
+          <view class="text u-line-1">购物车</view>
+        </view>
+      </view>
+      <view class="right">
+        <view class="cart btn u-line-1">加入购物车</view>
+        <view class="buy btn u-line-1">立即购买{{username}}</view>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -26,8 +65,13 @@ export default {
   data() {
     return {
       // 商品详情对象
-      detailList: [],
+      detailList: null,
     };
+  },
+  computed: {
+    username() {
+      return this.$store.state.username;
+    },
   },
   onLoad(option) {
     const goods_id = option.goods_id || 43984;
@@ -57,5 +101,52 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+.navigation {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  // 块级元素加固定或者绝对定位后  行内块 宽度 由内容撑开
+  width: 100%;
+
+  display: flex;
+  margin-top: 100rpx;
+  border: solid 2rpx #f2f2f2;
+  background-color: #ffffff;
+  padding: 16rpx 0;
+  .left {
+    display: flex;
+    font-size: 20rpx;
+    .item {
+      margin: 0 30rpx;
+      &.car {
+        text-align: center;
+        position: relative;
+        .car-num {
+          position: absolute;
+          top: -10rpx;
+          right: -10rpx;
+        }
+      }
+    }
+  }
+  .right {
+    display: flex;
+    font-size: 28rpx;
+    align-items: center;
+    .btn {
+      line-height: 66rpx;
+      padding: 0 30rpx;
+      border-radius: 36rpx;
+      color: #ffffff;
+    }
+    .cart {
+      background-color: #ed3f14;
+      margin-right: 30rpx;
+    }
+    .buy {
+      background-color: #ff7900;
+    }
+  }
+}
 </style>
