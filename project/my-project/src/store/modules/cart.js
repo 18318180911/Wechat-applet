f0f07de96cd2d18367054e2860ce1c3ae2a47131
18318180911 <80408028+18318180911@users.noreply.github.com>
2022-07-08 17:12:25 +0800

    商品详情-完成了商品的添加
    商品详情-完成了商品的添加

--- project/my-project/src/store/modules/cart.js
+++ project/my-project/src/store/modules/cart.js
@@ -8,7 +8,16 @@ export default {
     mutations: {
         // 添加数据到购物车数组中
         cartAddGoods(state, payload) {
-            state.goodsList.push(payload);
+            // state.goodsList.push(payload);
+            const index = state.goodsList.findIndex(
+                (goods) => goods.goods_id === playload.goods_id
+            );
+            if (index !== -1) {
+                state.goodsList[index].nums++;
+            } else {
+                // 不存在
+                state.goodsList.push(playload)
+            }
             console.log(12, state.goodsList);
         }
     },
